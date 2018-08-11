f99c755732c1127f98248035faf6ea8f7aa2b0b8
Arti Kearney <artemis.wk@gmail.com>
2018-08-11 10:02:17 -0700

    Plugin working

---
+++ HitScoreVisualizer/Harmony Patches/FlyingScoreTextEffectHandleSaberAfterCutSwingRatingCounterDidChangeEvent.cs
@@ -0,0 +1,29 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HitScoreVisualizer.Harmony_Patches
+{
+    [HarmonyPatch(typeof(FlyingScoreTextEffect), "HandleSaberAfterCutSwingRatingCounterDidChangeEvent",
+        new Type[] { typeof(SaberAfterCutSwingRatingCounter), typeof(float) })]
+    class FlyingScoreTextEffectHandleSaberAfterCutSwingRatingCounterDidChangeEvent
+    {
+        static void Postfix(SaberAfterCutSwingRatingCounter afterCutRating, FlyingScoreTextEffect __instance,
+            ref Color ____color, NoteCutInfo ____noteCutInfo, int ____multiplier)
+        {
+            ScoreController.ScoreWithoutMultiplier(____noteCutInfo, afterCutRating, out int before, out int after);
+            int total = before + after;
+            ____color =
+                (
+                    total < 100 ? Color.red :
+                    total <= 105 ? new Color(1, 165f / 255f, 0) :
+                    total < 110 ? new Color(0, 0.5f, 1) :
+                    Color.green
+                );
+        }
+    }
+}
---
+++ HitScoreVisualizer/Plugin.cs
@@ -0,0 +1,61 @@
+﻿using Harmony;
+using IllusionPlugin;
+using System;
+using System.Reflection;
+using UnityEngine.SceneManagement;
+
+namespace HitScoreVisualizer
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "HitScoreVisualizer";
+        public string Version => "0.0.1";
+        public void OnApplicationStart()
+        {
+            SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+            try
+            {
+                var harmony = HarmonyInstance.Create("com.arti.BeatSaber.HitScoreVisualizer");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[HitScoreVisualizer] This plugin requires Harmony. Make sure you " +
+                    "installed the plugin properly, as the Harmony DLL should have been installed with it.");
+                Console.WriteLine(e);
+            }
+        }
+
+        private void SceneManagerOnActiveSceneChanged(Scene arg0, Scene arg1)
+        {
+        }
+
+        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+        }
+
+        public void OnApplicationQuit()
+        {
+            SceneManager.activeSceneChanged -= SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+    }
+}
