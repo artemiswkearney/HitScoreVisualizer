c5281e4696005bc03d5a0d1ae2a9137ecb75b5e2
Eris <erisapps@outlook.be>
2020-10-08 00:46:34 +0200

    Fixed some weirdly written messages regarding the config state.

--- HitScoreVisualizer/Models/ConfigFileInfo.cs
+++ HitScoreVisualizer/Models/ConfigFileInfo.cs
@@ -19,11 +19,11 @@ namespace HitScoreVisualizer.Models
 		[UIValue("config-description")]
 		public string ConfigDescription => State switch
 		{
-			ConfigState.NewerVersion => $"<color=\"red\">Config is too new. Targets of version {Version}",
+			ConfigState.NewerVersion => $"<color=\"red\">Config is too new. Targets version {Version}",
 			ConfigState.Compatible => $"<color=\"green\">OK - {Version}",
 			ConfigState.NeedsMigration => $"<color=\"orange\">Config made for HSV {Version}. Migration possible.",
 			ConfigState.ValidationFailed => "<color=\"red\">Validation failed, please check the file again.",
-			ConfigState.Incompatible => $"<color=\"red\">Config is too old. Targets of version {Version}",
+			ConfigState.Incompatible => $"<color=\"red\">Config is too old. Targets version {Version}",
 			ConfigState.Broken => "<color=\"red\">Invalid config. Not selectable...",
 			_ => throw new NotImplementedException()
 		};
